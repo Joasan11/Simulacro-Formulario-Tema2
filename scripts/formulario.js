24fb9fd5724e40ef0eae3ccbe2c4db46ad95adb8
Joaquin Sanchez <joaquinsanchezmiravalles@alumnos.tuniversformacion.es>
2023-10-06 13:10:36 +0200

    Actualizacion js

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -8,7 +8,24 @@ document.getElementById("Registro").addEventListener("click", function (event) {
     const email = document.getElementById("Email").value.trim();
     const contrasena = document.getElementById("Contraseña").value.trim();
 
-    if (nombre != "" && apellidos != "" && email != "" && contrasena != "") {
+    if (nombre != "" && apellidos != "" && email != "" && contrasena.length >= 8) {
         alert("Te has registrado correctamente");
-    } 
+        //Borramos los campos si todo es correcto
+        document.getElementById("Nombre").value = "";
+        document.getElementById("Apellidos").value = "";
+        document.getElementById("Email").value = "";
+        document.getElementById("Contraseña").value = "";
+    }
+    else if (nombre === "") {
+        alert("El nombre no puede estar vacio");
+    }
+    else if (apellidos === "") {
+        alert("El apellido no puede estar vacio");
+    }
+    else if (email === "") {
+        alert("El email no puede estar vacio");
+    }
+    else if (contrasena.length < 8) {
+        alert("La contraseña necesita tener al menos 8 caracteres");
+    }
 });
